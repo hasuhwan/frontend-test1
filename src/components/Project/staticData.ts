88567a59ee803d6f566a9eeb2050995d479fad10
hasuhwan <hapung215@gmail.com>
2023-07-19 14:33:55 +0900

    절대 경로 수정

--- src/components/Project/staticData.ts
+++ src/components/Project/staticData.ts
@@ -6,13 +6,13 @@ interface IprojectData {
   stackoverflow: dataValue;
 }
 export const projectData: IprojectData = {
-  twitter: { imgSrc: "/twitter/twitterHome.png", title: "SNS 기본 구현하기" },
+  twitter: { imgSrc: "twitter/twitterHome.png", title: "SNS 기본 구현하기" },
   challenge: {
-    imgSrc: "/challenge/challengeHome.png",
+    imgSrc: "challenge/challengeHome.png",
     title: "66일 습관 만들기",
   },
   stackoverflow: {
-    imgSrc: "/stackoverflow/stackoverflowHome.png",
+    imgSrc: "stackoverflow/stackoverflowHome.png",
     title: "Stackoverflow Cloning",
   },
 };
--- src/components/viewProject/staticData.ts
+++ src/components/viewProject/staticData.ts
@@ -33,14 +33,14 @@ const viewProjectData: IviewProjectData = {
   },
   challenge: {
     img: [
-      "/challenge/challengeHomePage.gif",
-      "/challenge/challengePostHabit.gif",
-      "/challenge/challengeHabit.gif",
-      "/challenge/challengePostAuth.gif",
-      "/challenge/challengeSearchHabit.gif",
-      "/challenge/challengeCategory.gif",
-      "/challenge/challengeReview.gif",
-      "/challenge/challengeMypage.gif",
+      "challenge/challengeHomePage.gif",
+      "challenge/challengeHabit.gif",
+      "challenge/challengePostHabit.gif",
+      "challenge/challengePostAuth.gif",
+      "challenge/challengeSearchHabit.gif",
+      "challenge/challengeCategory.gif",
+      "challenge/challengeReview.gif",
+      "challenge/challengeMypage.gif",
     ],
     description: {
       진행기간: "2023.01.03 ~ 2023.02.02",
